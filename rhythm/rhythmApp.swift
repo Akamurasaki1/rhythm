819d99ac0cdbda8d843b08a7a69fbab903161661
Akamurasaki <karen.a.naito0803@gmail.com>
2025-11-15 18:35:26 +0900

    Initial Commit

---
+++ rhythm/rhythmApp.swift
@@ -0,0 +1,17 @@
+//
+//  rhythmApp.swift
+//  rhythm
+//
+//  Created by Karen Naito on 2025/11/15.
+//
+
+import SwiftUI
+
+@main
+struct rhythmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
